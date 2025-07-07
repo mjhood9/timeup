feab6cabeabbdc6d27c76a5850ff411cc0285aba
mjhood9 <jihane.mouadd@gmail.com>
2025-07-07 13:27:50 +0100

    first commit

---
+++ src/main/java/com/example/timeup/TimeupApplication.java
@@ -0,0 +1,13 @@
+package com.example.timeup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimeupApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/timeup/TimeupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.timeup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
